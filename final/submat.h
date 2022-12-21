da43ccb555d65796bf27997b011fc3a5bb1695c5
Rrytel <89410978+Rrytel@users.noreply.github.com>
2022-12-20 20:22:17 -0800

    Create submat.h

---
+++ final/submat.h
@@ -0,0 +1,60 @@
+#ifndef SUBMAT_H
+#define SUBMAT_H
+#include "mat.h"
+/* This class only is available on the GPU
+   Gets the BLOCK_SIZE x BLOCK_SIZE submatrix of a matrix that is
+   located col sub-matrices to the right and row sub-matrices down
+   from the upper-left corner of A */
+
+class subMatrix : public Matrix {
+public:
+  /* Member Data */
+  int stride;
+  float *elements;
+
+  __device__ subMatrix(GpuMatrix A, int sub_size, int row, int col) {
+    width = sub_size;
+    height = sub_size;
+    stride = A.width;
+    // memory at spot
+    elements = &A.elements[stride * width * row + height * col];
+  }
+  // Get matrix element
+  __device__ inline float GetElem(const int row, const int col) {
+    return elements[row * stride + col];
+  }
+
+  // Set a matrix element
+  __device__ inline void SetElem(const int row, const int col,
+                                 const float value) {
+    elements[row * stride + col] = value;
+  }
+};
+
+class subVector : public Vector {
+public:
+  /* Member Data */
+  int stride;
+  float *elements;
+
+  __device__ subVector(GpuVector A, int sub_size, int row) {
+    width = sub_size;
+    stride = A.width;
+    // memory at spot
+    elements = &A.elements[sub_size * row];
+    
+    
+  }
+  // Get matrix element
+  __device__ inline float GetElem(const int row) {
+     return elements[row];
+  }
+
+  // Set a matrix element
+  __device__ inline void SetElem(const int row,
+                                 const float value) {
+     elements[row] = value;
+    
+  }
+};
+#endif
